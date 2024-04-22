c3e2627f54089cc73aa5c63a8f4ac5be3f4ebd5f
Nikhil Devadoss <nikdevadoss@gmail.com>
2024-04-21 17:47:34 -0700

    Favicon update

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -11,13 +11,13 @@ import { Header } from '@/components/header'
 export const metadata = {
   metadataBase: new URL(`https://${process.env.VERCEL_URL}`),
   title: {
-    default: 'Next.js AI Chatbot',
-    template: `%s - Next.js AI Chatbot`
+    default: 'Athena',
+    template: `%s`
   },
-  description: 'An AI-powered chatbot template built with Next.js and Vercel.',
+  description: 'Athena',
   icons: {
     icon: '/favicon.ico',
-    shortcut: '/favicon-16x16.png',
+    shortcut: '/athena-logo-transparent.png',
     apple: '/apple-touch-icon.png'
   }
 }
